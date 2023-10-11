3ef889f43531e94a84b07f3c414aa1f97df35d9f
Danielcode11 <danielserrator11@hotmail.com>
2023-10-10 21:58:03 -0500

    Agrego Vista Productos

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,10 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
+
+
 export class ProductosComponent {
+
+  private dataUrl = 'assets/productos.json';
+
+  productos: any[]=[];
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any[]>(this.dataUrl);
+  }
+
+  searchProductByName(name: string): Observable<any[]> {
+    return this.http.get<any[]>(this.dataUrl).pipe(
+      map((data: any[]) => data.filter(product => product.name.toLowerCase().includes(name.toLowerCase())))
+    );
+  }
+
+  ngOnInit(): void {
+    this.getData().subscribe(data => {
+      this.productos = data.productos;
+      console.log(this.productos);
+    });
+  }
 
 }
